a099439f8185e8b88b1eac28de980ce5b447fda1
Gabriel Ladeira <gabriellopes7@yahoo.com>
2023-06-03 18:55:06 -0300

    chapter9

---
+++ chapter9mvc/server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const {logger} = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
+const PORT = process.env.PORT || 3500
+
+//custom middleware logger
+app.use(logger);
+
+//WHITE LIST - application domains that can access this backend node server
+
+//CORS - Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+//built-in middleware to handle urlencoded data or form data
+//'content-type': application/x-www-form-urlencoded'
+app.use(express.urlencoded({extended: false}))
+
+//built-in middleware for json
+app.use(express.json())
+
+//serve static files
+//enables public folder, css, img, etc
+app.use('/',express.static(path.join(__dirname,'/public')))
+
+app.use('/', require('./routes/root'))
+app.use('/employees',require('./routes/api/employees'))
+
+
+//app.use('/')
+//Handling 404 errors
+app.all('*',(req,res) =>{
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }
+    else if(req.accepts('json')){
+        res.json({error: "404 Not Found"})
+    }
+    else {
+        res.type('txt').send("404 Not Found")
+    }
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, () => 
+console.log(`Server running on port ${PORT}`))
